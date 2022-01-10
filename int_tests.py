b21bdd6de1669ec3e70f05f87b2fd10b8171598e
Scott Cyphers <cyphers@lightmatter.co>
2022-01-10 10:14:31 -0800

    Make tests similar to glow's

--- int_tests.py
+++ int_tests.py
@@ -2,19 +2,35 @@ import onnx
 import numpy as np
 from builder import *
 
+_camelName = {
+    np.int8: 'Int8',
+    np.uint8: 'UInt8',
+    np.int16: 'Int16',
+    np.uint16: 'UInt16',
+    np.int32: 'Int32',
+    np.int64: 'Int64'
+}
+
+
+def camel_name(typ):
+    return _camelName[typ]
+
 
 def run():
     input = Placeholder()
 
     for typ in (np.int8, np.uint8, np.int16, np.uint16):
         exporter = Exporter()
-        exporter.add_graph_input('input', input, shape=[1, 2, 3], elt_type=typ)
-        exporter.add_graph_output('output', Cast(
-            input + Constant(np.ones([1, 2, 3], dtype=typ)), np.float32))
+        exporter.add_graph_input('data',
+                                 input,
+                                 shape=[1, 2, 2, 2],
+                                 elt_type=typ)
+        exporter.add_graph_output('output', Cast(input, np.float32))
         md = exporter.export(f'{typ.__name__}_to_float')
-        with open(f'{typ.__name__}_to_float.onnxtxt', 'w') as f:
+        fname = f'cast{camel_name(typ)}ToFloat'
+        with open(f'{fname}.onnxtxt', 'w') as f:
             f.write(str(md))
-        onnx.save(md, f'{typ.__name__}_to_float.onnx')
+        onnx.save(md, f'{fname}.onnx')
 
 
 if __name__ == "__main__":
